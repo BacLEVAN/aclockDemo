375faf872bcd49707dfec5e83903723cb6b1bbb0
BacLEVAN <baclevan@laptrinh.vn>
2016-12-22 17:19:27 +0700

    - Thực hiện up code để build app lần thứ 7 - Thêm chức năng chơi file nhạc .mp3

--- www/js/experiment.js
+++ www/js/experiment.js
@@ -24,8 +24,80 @@ $(function(){
             $('#second')[0].style[prop] = 'rotate('+angle * second+'deg)';
             $('#hour')[0].style[prop] = 'rotate('+hourAngle+'deg)';
         }
+
     }
 
-    // Thực hiện chạy đồng hồ ngay khi load trang
-    setInterval(function(){startClock();}, 500);
+    // Thực hiện chạy đồng hồ ngay khi load trang, thực hiện chạy với tốc độ 5 milisecond
+    setInterval(
+        function(){
+            startClock();
+    }, 100);
+
+    /*
+    function autoPlay() {
+        $("#video_player")[0].autoplay = true;
+    }
+
+    setInterval(
+        function(){
+            autoPlay();
+    }, 5000);
+    date = new Date();
+    console.log(date.getHours());
+    console.log(date.getMinutes());
+    console.log(date.getSeconds());
+    */
+
+    var testObject = {
+        'one': 5000, // 5 second
+        'two': 15000, 
+        'three': 25000,
+        'four': 35000,
+        'five': 45000,
+        'six': 55000 // 55 second
+      };
+    // Put the object into storage
+    localStorage.setItem('testObject', JSON.stringify(testObject));
+
+    // Retrieve the object from storage
+    var retrievedObject = localStorage.getItem('testObject');
+
+    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+    function autoPlay() {
+        // $("#video_player")[0].autoplay = true;
+        $("#video_player")[0].play();
+    }
+
+    function autoStop() {
+        $("#video_player")[0].pause();
+    }
+
+    var setSecond = JSON.parse(retrievedObject);
+
+    setTimeout(function() {
+        autoPlay();
+    }, setSecond.one);
+
+    setTimeout(function() {
+       autoStop();
+    }, setSecond.two);
+
+    setTimeout(function() {
+       autoPlay();
+    }, setSecond.three);
+
+    setTimeout(function() {
+       autoStop();
+    }, setSecond.four);
+
+
+    setTimeout(function() {
+       autoPlay();
+    }, setSecond.five);
+
+    setTimeout(function() {
+       autoPlay();
+    }, setSecond.six);
+
 });
